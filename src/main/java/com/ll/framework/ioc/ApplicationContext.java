09614a07cdf48ec5554d1544ff50259b6c463d66
Park-seungg <parkis1101@gmail.com>
2025-08-11 18:00:02 +0900

    test 2,3,4,5 - green

--- src/main/java/com/ll/framework/ioc/ApplicationContext.java
+++ src/main/java/com/ll/framework/ioc/ApplicationContext.java
@@ -1,11 +1,36 @@
 package com.ll.framework.ioc;
 
-public class ApplicationContext {
-    public ApplicationContext() {
+import com.ll.domain.testPost.testPost.repository.TestPostRepository;
+import com.ll.domain.testPost.testPost.service.TestPostService;
 
+import java.util.HashMap;
+import java.util.Map;
+
+public class ApplicationContext {
+    private final Map<String, Object> beans = new HashMap<>();
+
+    public ApplicationContext() {
+        // 생성자: 여전히 빈 로직
     }
 
     public <T> T genBean(String beanName) {
-        return (T) null;
+
+        if (beans.containsKey(beanName)) {
+            return (T) beans.get(beanName);
+        }
+
+        Object bean = null;
+        if (beanName.equals("testPostRepository")) {
+            bean = new TestPostRepository();
+        } else if (beanName.equals("testPostService")) {
+            TestPostRepository repo = genBean("testPostRepository");
+            bean = new TestPostService(repo);
+        }
+
+        if (bean != null) {
+            beans.put(beanName, bean);
+        }
+
+        return (T) bean;
     }
 }
