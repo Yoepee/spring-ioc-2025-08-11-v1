f367e3613ba5a642c9decef7971480d07922762f
Yoepee <kdy2975@naver.com>
2025-08-11 16:48:25 +0900

    feat: genBean 생성

--- src/main/java/com/ll/framework/ioc/ApplicationContext.java
+++ src/main/java/com/ll/framework/ioc/ApplicationContext.java
@@ -1,11 +1,43 @@
 package com.ll.framework.ioc;
 
-public class ApplicationContext {
-    public ApplicationContext() {
+import com.ll.domain.testPost.testPost.repository.TestPostRepository;
+import com.ll.domain.testPost.testPost.service.TestFacadePostService;
+import com.ll.domain.testPost.testPost.service.TestPostService;
 
-    }
+public class ApplicationContext {
+    private static TestPostService testPostService;
+    private static TestPostRepository testPostRepository;
+    private static TestFacadePostService testFacadePostService;
+
+    public ApplicationContext() {}
 
     public <T> T genBean(String beanName) {
-        return (T) null;
+        // beanName에 따라 해당 빈을 반환
+        switch (beanName) {
+            case "testPostService":
+                if (testPostService == null) {
+                    testPostService = new TestPostService(
+                            genBean("testPostRepository")
+                    );
+                }
+                return (T) testPostService;
+
+            case "testPostRepository":
+                if (testPostRepository == null) {
+                    testPostRepository = new TestPostRepository();
+                }
+                return (T) testPostRepository;
+
+            case "testFacadePostService":
+                if (testFacadePostService == null) {
+                    testFacadePostService = new TestFacadePostService(
+                            genBean("testPostService"),
+                            genBean("testPostRepository")
+                    );
+                }
+                return (T) testFacadePostService;
+            default:
+                return null;
+        }
     }
 }
