12167a9bdc0b1b2753f3adb8be07d765a68b91f9
GeunYeong <jgy091411@gmail.com>
2025-08-14 16:55:33 +0900

    V1

--- src/main/java/com/ll/domain/testPost/testPost/service/TestPostService.java
+++ src/main/java/com/ll/domain/testPost/testPost/service/TestPostService.java
@@ -3,7 +3,18 @@ package com.ll.domain.testPost.testPost.service;
 import com.ll.domain.testPost.testPost.repository.TestPostRepository;
 import lombok.RequiredArgsConstructor;
 
-@RequiredArgsConstructor
 public class TestPostService {
     private final TestPostRepository testPostRepository;
+
+    private TestPostService(TestPostRepository testPostRepository) {
+        this.testPostRepository = testPostRepository;
+    }
+
+    private static class InstanceHolder {
+        private static final TestPostService INSTANCE = new TestPostService(TestPostRepository.getInstance());
+    }
+
+    public static TestPostService getInstance() {
+        return InstanceHolder.INSTANCE;
+    }
 }
